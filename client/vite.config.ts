9461044470e6b3f569e53e2b1321ef23c682ddd1
MTrazona <trazonamichael.pdm@gmail.com>
2024-10-05 18:56:19 +0800

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,6 +10,7 @@ export default {
       allow: [
         "C:/Users/acer/Documents/GitHub/freelance/health-center",
         "C:/Users/acer/Documents/proj1/client>",
+        "D:/Freelance/health-center/health-center/client"
       ],
     },
   },
